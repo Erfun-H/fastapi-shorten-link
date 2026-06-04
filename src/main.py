871e9c04643e5b7fdd429fdf27742379d250cf56
erfan <efih306@gmail.com>
2026-06-04 17:40:38 +0330

    refactor: customize request validation error responses.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+
 from src.shortener.routers import shortener_router
 
 import os
@@ -6,3 +9,23 @@ import os
 app = FastAPI(debug=os.getenv("DEBUG"))
 
 app.include_router(shortener_router, tags=["shortener"])
+
+
+@app.exception_handler(RequestValidationError)
+async def request_validation_handler(request: Request, exc: RequestValidationError):
+    errors = [
+        {
+            "type": error.get("type"),
+            "field": error.get("loc")[-1],
+            "message": error.get("msg"),
+            "input": error.get("input")
+        }
+        for error in exc.errors()
+    ]
+
+    return JSONResponse(
+        status_code=400,
+        content={
+            "errors": errors
+        }
+    )
